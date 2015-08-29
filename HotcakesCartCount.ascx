b27fd8d2026d879dddf52863ca65493951f9456f
David Poindexter <dpoindexter@nvisionative.com>
2015-08-29 14:36:50 -0400

    Initial commit.

---
+++ HotcakesCartCount.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="vb" AutoEventWireup="true" CodeBehind="HotcakesCartCount.ascx.vb" Inherits="DNNSkinOjectHotcakesCartCount.HotcakesCartCount" %>
+<asp:Label ID="lblCartCount" runat="server" CssClass="Nomal"></asp:Label>
---
+++ HotcakesCartCount.ascx.vb
@@ -0,0 +1,20 @@
+﻿Imports Hotcakes.Commerce.Extensions
+Imports Hotcakes.Commerce.Orders
+
+Public Class HotcakesCartCount
+    Inherits System.Web.UI.UserControl
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        'Create instance of Hotcakes Commerce
+        Dim HccApp = HccAppHelper.InitHccApp()
+
+        'Get current cart
+        Dim currentCart = HccApp.OrderServices.CurrentShoppingCart
+
+        'Populate label with current cart item count 
+        lblCartCount.Text = currentCart.Items.Count.ToString
+
+    End Sub
+
+End Class
